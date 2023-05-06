0065a0b16686e5853b5db905f83de0e69cc78295
Ian Mullins <imullins@horizon.csueastbay.edu>
2023-05-05 18:27:57 -0700

    img command added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 
+
 dotenv.config();
 
 
@@ -12,6 +13,7 @@ const res = require('express/lib/response');
 const app = express()
 const PORT = 8001
 const { Client , Events} = require('discord.js-selfbot-v13');
+const { channel } = require('node:diagnostics_channel');
 
 
 const client = new Client({
@@ -48,11 +50,64 @@ app.post("/addPrompt", (request,response)=>{
 })
 
 
-// client.on('messageCreate', async (message) => {
-// 	if (message.author.id == 'Midjourney ID' && message.embeds && message.embeds[0].image) {
-// 		image = message.embeds[0].image.url
-// 		const channel = client.channels.cache.get('Channel ID')
-// 		channel.send(message.content + '\n\n' + image)
-// 	  }
-// 	}) 
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+client.on("messageCreate", async (message) => {
+	if (message.author.id == '936929561302675456'){
+	 console.log(message.attachments.first())
+	 console.log(`Message from ${message.author.username}: ${message.content}`)
+	 console.log('------')
+	 id = message.id
+	 //disabled for bobby
+	//  channel = message.channelId
+	 console.log(message.attachments.size)
+	}
+	if (message.attachments.size==1){
+		console.log(message.attachments.first().url)
+		image = message.attachments.first()?.url
+		//disabled for bobby
+		// channel.message(image)
+
+	}
 	
+	if (message.author.id !='936929561302675456'){
+		if (message.content.startsWith('!img')){
+			var test = client.channels.cache.get('1103168663617556571')
+			test.sendSlash('936929561302675456','imagine', message.content.slice(5))
+			
+		}
+	}
+	
+
+	// await sleep(10000)
+	// console.log(message.attachments.first().url)
+	// image = message.attachments.first()?.url
+	
+	// channel = client.channels.cache.get(channel);
+}
+	
+	
+	
+	// message = await channel.messages.fetch(id);
+	// // console.log(message)
+	// console.log(message.attachments)
+	// console.log('------')
+	// console.log(message.attachments.first()?.url)
+	// image = message.attachments.first()?.url
+
+
+
+	
+	
+  );
+  
+//   client.login(process.env.BOT_TOKEN);
+
+
+
+
